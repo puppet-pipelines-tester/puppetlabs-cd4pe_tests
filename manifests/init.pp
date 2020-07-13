cb7fcaa43e32b7a52e0c12434f0b78f7bb8b4fd6
puppet-pipelines-tester <40370030+puppet-pipelines-tester@users.noreply.github.com>
2020-07-13 14:52:41 -0700

    Update init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -44,6 +44,6 @@
 #
 class cd4pe_tests {
   notify { "test_notify":
-    message => "notify from puppetlabs-cd4pe_tests#master 3.10.0 tests",
+    message => "notify from puppetlabs-cd4pe_tests#master 3.12.0 tests",
   }
 }
